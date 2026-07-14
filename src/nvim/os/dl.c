7b507d352ddd3ef80c5b677797eedb43f9b6d84a
Roberto Canha <robertocanha@ocsecurity.pt>
2026-07-14 13:45:43 +0100

    dl

--- src/nvim/os/dl.c
+++ src/nvim/os/dl.c
@@ -36,51 +36,51 @@ typedef int (*int_int_fn)(int i);
 ///             not NULL. NULL when using `int_out`.
 /// @param[out] int_out the output integer param
 /// @return true on success, false on failure
-bool os_libcall(const char *libname, const char *funcname, const char *argv, int argi,
-                char **str_out, int *int_out)
-{
-  if (!libname || !funcname) {
-    return false;
-  }
+//bool os_libcall(const char *libname, const char *funcname, const char *argv, int argi,
+//                char **str_out, int *int_out)
+//{
+//  if (!libname || !funcname) {
+//    return false;
+//  }
 
-  uv_lib_t lib;
+//  uv_lib_t lib;
 
   // open the dynamic loadable library
-  if (uv_dlopen(libname, &lib)) {
-    semsg(_("dlerror = \"%s\""), uv_dlerror(&lib));
-    uv_dlclose(&lib);
-    return false;
-  }
+//  if (uv_dlopen(libname, &lib)) {
+//    semsg(_("dlerror = \"%s\""), uv_dlerror(&lib));
+//    uv_dlclose(&lib);
+//    return false;
+//  }
 
   // find and load the requested function in the library
-  gen_fn fn;
-  if (uv_dlsym(&lib, funcname, (void **)&fn)) {
-    semsg(_("dlerror = \"%s\""), uv_dlerror(&lib));
-    uv_dlclose(&lib);
-    return false;
-  }
+//  gen_fn fn;
+//  if (uv_dlsym(&lib, funcname, (void **)&fn)) {
+//    semsg(_("dlerror = \"%s\""), uv_dlerror(&lib));
+//    uv_dlclose(&lib);
+//    return false;
+//  }
 
   // call the library and save the result
   // TODO(aktau): catch signals and use jmp (if available) to handle
   // exceptions. jmp's on Unix seem to interact trickily with signals as
   // well. So for now we only support those libraries that are well-behaved.
-  if (str_out) {
-    str_str_fn sfn = (str_str_fn)fn;
-    int_str_fn ifn = (int_str_fn)fn;
+//  if (str_out) {
+//    str_str_fn sfn = (str_str_fn)fn;
+//    int_str_fn ifn = (int_str_fn)fn;
 
-    const char *res = argv ? sfn(argv) : ifn(argi);
+//    const char *res = argv ? sfn(argv) : ifn(argi);
 
     // assume that ptr values of NULL, 1 or -1 are illegal
-    *str_out = (res && (intptr_t)res != 1 && (intptr_t)res != -1)
-               ? xstrdup(res) : NULL;
-  } else {
-    str_int_fn sfn = (str_int_fn)fn;
-    int_int_fn ifn = (int_int_fn)fn;
-    *int_out = argv ? sfn(argv) : ifn(argi);
-  }
+//    *str_out = (res && (intptr_t)res != 1 && (intptr_t)res != -1)
+//               ? xstrdup(res) : NULL;
+//  } else {
+//    str_int_fn sfn = (str_int_fn)fn;
+//    int_int_fn ifn = (int_int_fn)fn;
+//    *int_out = argv ? sfn(argv) : ifn(argi);
+//  }
 
   // free the library
-  uv_dlclose(&lib);
+//  uv_dlclose(&lib);
 
-  return true;
-}
+//  return true;
+//}
--- src/nvim/os/dl.h
+++ src/nvim/os/dl.h
@@ -3,3 +3,5 @@
 #include <stdbool.h>
 
 #include "os/dl.h.generated.h"
+bool os_libcall(const char *libname, const char *funcname, const char *argv, int argi,
+                char **str_out, int *int_out);
